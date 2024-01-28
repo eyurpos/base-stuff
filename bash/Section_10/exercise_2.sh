64ea017e14294b11617fd770a42ae6886d421330
Yurii Posin <necro.twenty.free@gmail.com>
2024-01-28 15:38:55 +0100

    Section 10 Bash cource

---
+++ bash/Section_10/exercise_2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+# Exercise 2:
+#
+# Modify the previous exercise so that script continues, 
+# even if an error occurs. This time all three ls commands will execute.                                                         
+
+ls /etc/passwd
+ls /move/along/nothing/to/see/here
+ls /etc/passwd
