68a6700c7fbb819639db2bf2a49c5bec6f69a6d6
Yurii Posin <necro.twenty.free@gmail.com>
2024-03-21 22:11:44 +0100

    Python base course

---
+++ python/python_base/Slicer.py
@@ -0,0 +1,11 @@
+
+word = "antidisestablishmentarianism"
+
+# use a slice to take out the word "establishment"
+# and store it in the answer variable....
+
+
+answer = word[len("antidis"):word.index("aria")]
+
+
+print(answer)
