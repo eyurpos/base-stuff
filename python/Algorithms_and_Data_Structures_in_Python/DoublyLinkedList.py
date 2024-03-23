5bad271384e5d87e3ec447a52301c63ddfc0a64e
Yurii Posin <necro.twenty.free@gmail.com>
2024-03-23 22:25:06 +0100

    Python course 2. part 2

---
+++ python/Algorithms_and_Data_Structures_in_Python/DoublyLinkedList.py
@@ -0,0 +1,154 @@
+# Node structure
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+    def __repr__(self):
+        return str(self.data)
+    
+# Doubly Linked List structure
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def push_back(self, data):
+        newEl = Node(data)
+
+        if not self.tail:
+            self.tail = newEl
+            self.head = self.tail
+        else:
+            self.tail.next = newEl
+            newEl.prev = self.tail
+            self.tail = newEl
+        self.size += 1
+
+    def push_top(self, data):
+        newEl = Node(data)
+
+        if not self.head:
+            self.head = newEl
+            self.tail = newEl
+        else:
+            newEl.next = self.head
+            self.head.prev = newEl
+            self.head = newEl
+
+        self.size += 1
+
+    def find_forward_el(self, position):
+        tmp = self.head
+        while position > 0:
+            tmp = tmp.next
+            position -= 1
+
+        return tmp
+
+    def find_backward_el(self, position):
+        tmp = self.tail
+        number = self.size-1
+        while number > position:
+            tmp = tmp.prev
+            number -= 1
+
+        return tmp
+
+    def insert(self, position, data):
+        newEl = Node(data)
+
+        if self.size <= position:
+            print("{} bigger actual size {}".format(position, self.size))
+            self.push_back(data)
+            return
+        
+        if position == 0:
+            self.push_top(data)
+            return
+        
+        tmp = None
+        if position < self.size//2:
+            print(1)
+            tmp = self.find_forward_el(position)
+        else:
+            print(2)
+            tmp = self.find_backward_el(position)
+
+        if not tmp:
+            print ("Error")
+            return
+        
+        newEl.next = tmp
+        newEl.prev = tmp.prev
+        newEl.next.prev = newEl
+        newEl.prev.next = newEl
+
+        self.size += 1
+
+    def remove(self, data):
+        if self.head == self.tail:
+            if self.head == data:
+                self.head = None
+                self.tail = None
+        elif self.head.data == data:
+            self.head = self.head.next
+            self.head.prev = None
+        elif self.tail.data == data:
+            self.tail = self.tail.prev
+            self.tail.next = None
+        else:
+            tmp = self.head
+            while tmp.next:
+                if tmp.next.data == data:
+                    tmp.next = tmp.next.next
+                    tmp.next.prev = tmp
+                    break
+                else:
+                    tmp = tmp.next
+
+        self.size -= 1
+
+
+    def size(self):
+        return self.size
+    
+    def print_forward(self):
+
+        print('List size {}:\n'.format(self.size))
+        tmp = self.head
+
+        while tmp is not None:
+            print(tmp)
+            tmp = tmp.next
+
+    def print_backward(self):
+
+        print('List size {}:\n'.format(self.size))
+        tmp = self.tail
+
+        while tmp is not None:
+            print(tmp)
+            tmp = tmp.prev
+
+if __name__ == '__main__':
+    doublyLinkedList = DoublyLinkedList()
+    doublyLinkedList.push_back(10)
+    doublyLinkedList.push_top(100)
+    doublyLinkedList.insert(5,1000)
+    doublyLinkedList.insert(0,3)
+    doublyLinkedList.push_back('Adam')
+    doublyLinkedList.push_back(7.5)
+    doublyLinkedList.print_forward()
+    print('-------')
+    doublyLinkedList.remove(1000)
+    doublyLinkedList.print_forward()
+    print('-------')
+    doublyLinkedList.insert(1,67)
+    doublyLinkedList.insert(4,68)
+    doublyLinkedList.print_forward()
+    print('-------')
+    doublyLinkedList.print_backward()
+    print('-------')
---
+++ python/Algorithms_and_Data_Structures_in_Python/LinkedList.py
@@ -0,0 +1,98 @@
+# Node structure
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def __repr__(self):
+        return str(self.data)
+    
+# Linked List structure
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def push_back(self, data):
+        newEl = Node(data)
+
+        if not self.head:
+            self.head = newEl
+        else:
+            tmp = self.head
+            while tmp.next:
+                tmp = tmp.next
+            tmp.next = newEl
+        self.size += 1
+
+    def push_top(self, data):
+        newEl = Node(data)
+
+        newEl.next = self.head
+        self.head = newEl
+        self.size += 1
+
+    def insert(self, position, data):
+        newEl = Node(data)
+
+        if self.size <= position:
+            print("{} bigger actual size {}".format(position, self.size))
+            self.push_back(data)
+            return
+        
+        if position == 0:
+            self.push_top(data)
+            return
+        
+        tmp = self.head
+        while position > 1:
+            tmp = tmp.next
+            position -= 1
+
+        newEl.next = tmp.next
+        tmp.next = newEl
+        self.size += 1
+
+    def remove(self, data):
+        if self.head.data == data:
+            self.head = self.head.next
+        else:
+            tmp = self.head
+            while tmp.next:
+                if tmp.next.data == data:
+                    tmp.next = tmp.next.next
+                    break
+                else:
+                    tmp = tmp.next
+
+        self.size -= 1
+
+
+    def size(self):
+        return self.size
+    
+    def print(self):
+
+        print('List size {}:\n'.format(self.size))
+        tmp = self.head
+
+        while tmp is not None:
+            print(tmp)
+            tmp = tmp.next
+
+if __name__ == '__main__':
+    linkedList = LinkedList()
+    linkedList.push_back(10)
+    linkedList.push_top(100)
+    linkedList.insert(5,1000)
+    linkedList.insert(0,3)
+    linkedList.push_back('Adam')
+    linkedList.push_back(7.5)
+    linkedList.print()
+    print('-------')
+    linkedList.remove(1000)
+    linkedList.print()
+    print('-------')
+    linkedList.insert(2,67)
+    linkedList.print()
+    print('-------')
